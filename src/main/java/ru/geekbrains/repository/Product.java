55d6a53780ecb24fd00de5203ef54ef5bf2993e9
Roman <novikovroman96@gmail.com>
2022-02-03 21:01:05 +0300

    homework added

---
+++ src/main/java/ru/geekbrains/repository/Product.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class Product {
+
+    private Long id;
+    private String title;
+    private BigDecimal cost;
+
+    public Product(Long id, String title, BigDecimal cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product {id = %-2s | name = %-15s | price = %-8s}", id, title, cost);
+    }
+}
